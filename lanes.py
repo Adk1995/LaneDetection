5ea2b9eb1cfbcd30933a13ae584754aad52aaf0e
Adhokshith <adhokshithdatta@gmail.com>
2018-11-13 15:33:28 -0600

    Video lane Analysis

--- lanes.py
+++ lanes.py
@@ -14,8 +14,41 @@ def observation_area(image):
     polygons =np.array([[(200,height),(1100,height),(550, 250)]])
     mask = np.zeros_like(image)
     cv2.fillPoly(mask, polygons, 255)
-    masked_image = cv2.bitwise_and(image,mask)
+    masked_image = cv2.bitwise_and(image , mask)
     return masked_image
+
+
+def average_slope_intercept(image, lines):
+    left_fit = []
+    right_fit = []
+    for line in lines:
+        x1, y1, x2, y2 = line.reshape(4)
+        parameters = np.polyfit((x1, x2),(y1, y2), 1)
+        slope = parameters[0]
+        intercept = parameters[1]
+        #left side has negative slope whereas right slope has positive slope
+        if slope <0:
+            left_fit.append((slope, intercept))
+        if slope>0:
+            right_fit.append((slope, intercept))
+    print(left_fit)
+    print(right_fit)
+    left_fit_average = np.average(left_fit, axis=0)
+    right_fit_average = np.average(right_fit, axis=0)
+    left_line = make_coordinates(image, left_fit_average)
+    right_line = make_coordinates(image, right_fit_average)
+    return np.array([left_line, right_line])
+
+def make_coordinates(image, line_params):
+    slope, intercept = line_params
+    print(image.shape)
+    y1 = image.shape[0]
+    y2 = int(y1*3/5)
+    x1 = int((y1 - intercept)/slope)
+    x2 = int((y2 - intercept)/slope)
+
+    return np.array([x1, y1, x2, y2])
+
 
 def display_lines(image, lines):
     line_image = np.zeros_like(image)
@@ -27,13 +60,23 @@ def display_lines(image, lines):
 
 
 
-image = cv2.imread('/Users/adk1995/Desktop/LaneDetection/image/test_image.jpg')
-lane_image = np.copy(image)
+#image = cv2.imread('/Users/adk1995/Desktop/LaneDetection/image/test_image.jpg')
+#lane_image = np.copy(image)
 
-canny = canny(lane_image)
-cropped_image = observation_area(canny)
-lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]),minLineLength=40, maxLineGap=5)
-line_image = display_lines(lane_image, lines)
-combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
-cv2.imshow('result', combo_image)
-cv2.waitKey(0)
+
+
+cap = cv2.VideoCapture('video/test2.mp4')
+while(cap.isOpened()):
+    _, frame = cap.read()
+    canny_image = canny(frame)
+    cropped_image = observation_area(canny_image)
+    lines = cv2.HoughLinesP(cropped_image, 2, np.pi / 180, 100, np.array([]), minLineLength=40, maxLineGap=5)
+    averaged_lines = average_slope_intercept(frame, lines)
+    line_image = display_lines(frame, averaged_lines)
+    combo_image = cv2.addWeighted(frame, 0.8, line_image, 1, 1)
+    cv2.imshow('result', combo_image)
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cap.destroyAllWindows()
