cc1888510c0f019de11bfd83cc00e8a4cd1a0363
Adhokshith <adhokshithdatta@gmail.com>
2018-11-13 11:48:40 -0600

    Overlayed lanes

--- lanes.py
+++ lanes.py
@@ -17,10 +17,23 @@ def observation_area(image):
     masked_image = cv2.bitwise_and(image,mask)
     return masked_image
 
+def display_lines(image, lines):
+    line_image = np.zeros_like(image)
+    if lines is not None:
+        for line in lines:
+            x1, y1, x2, y2 = line.reshape(4)
+            cv2.line(line_image, (x1,y1),(x2,y2), (255,0,0), 10)
+    return line_image
+
+
+
 image = cv2.imread('/Users/adk1995/Desktop/LaneDetection/image/test_image.jpg')
 lane_image = np.copy(image)
 
 canny = canny(lane_image)
 cropped_image = observation_area(canny)
-cv2.imshow('result',cropped_image)
+lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]),minLineLength=40, maxLineGap=5)
+line_image = display_lines(lane_image, lines)
+combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
+cv2.imshow('result', combo_image)
 cv2.waitKey(0)
